c44415656d7c995e2c95bcb75a1296b9e38e618a
NobleLip <skate777771@hotmail.com>
2021-06-28 18:15:36 +0100

    Livros Added

---
+++ app/src/main/java/ipg/pt/trabalhofinal/Livros.kt
@@ -0,0 +1,38 @@
+package ipg.pt.trabalhofinal
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+import pt.ipg.trabalhofinal.TabelaLivros
+
+
+data class Livros (var id: Long = -1, var Nome: String, var Avalia: Int, var Tipo: String) {
+
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues().apply {
+            put(TabelaLivros.NOME_LIVRO, Nome)
+            put(TabelaLivros.AVALIA, Avalia)
+            put(TabelaLivros.TIPO, Tipo)
+        }
+
+        return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Livros {
+
+            val posCampoId = cursor.getColumnIndex(BaseColumns._ID)
+            val posCampoNomeLivro = cursor.getColumnIndex(TabelaLivros.NOME_LIVRO)
+            val posCampoAvalia = cursor.getColumnIndex(TabelaLivros.AVALIA)
+            val posCampoTipo = cursor.getColumnIndex(TabelaLivros.TIPO)
+
+
+            val id = cursor.getLong(posCampoId)
+            val Nome = cursor.getString(posCampoNomeLivro)
+            val Avalia = cursor.getInt(posCampoAvalia)
+            val Tipo = cursor.getString(posCampoTipo)
+
+            return Livros(id, Nome, Avalia, Tipo)
+        }
+    }
+}
