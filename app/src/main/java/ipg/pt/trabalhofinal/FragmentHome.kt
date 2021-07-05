7d403010fa56a1361ebcd1e1b556051ed68e19dc
NobleLip <skate777771@hotmail.com>
2021-07-05 13:26:00 +0100

    Adicinados Fragmentos

--- app/src/main/java/ipg/pt/trabalhofinal/FragmentHome.kt
+++ app/src/main/java/ipg/pt/trabalhofinal/FragmentHome.kt
@@ -6,14 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import ipg.pt.trabalhofinal.databinding.FragmentSecondBinding
+import ipg.pt.trabalhofinal.databinding.FragmentHomeBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+class FragmentHome : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +22,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +30,16 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.buttonLivros.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentHomee_to_fragmentLivros)
+        }
+
+        binding.buttonPaises.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentHomee_to_fragmentLivros)
+        }
+
+        binding.buttonPessoas.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentHomee_to_fragmentLivros)
         }
     }
 
